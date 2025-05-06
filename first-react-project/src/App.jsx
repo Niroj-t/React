6111c29a4214852f9daf1d8dd351a48933dbd4a5
Niroj_t <nirojthapa661@gmail.com>
2025-05-06 14:32:35 +0545

    initial commit

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <p>This is my first react project</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ first-react-project/src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav'><button>This is my Navbar</button></div>
+    </div>
+  )
+}
+
+export default Navbar
